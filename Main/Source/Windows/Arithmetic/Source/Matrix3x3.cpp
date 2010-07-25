236ee186a1b69903ce996576e7f35ab925115843
teh_programerer <devnull@localhost>
2010-07-25 03:31:27 +0100

    Removed XNA version, as the project was not correctly configured.  It will return in the next commit.

--- Main/Source/Windows/Arithmetic/Source/Matrix3x3.cpp
+++ Main/Source/Windows/Arithmetic/Source/Matrix3x3.cpp
@@ -24,8 +24,20 @@ namespace ZED
 		void Matrix3x3::Rotate( const ZED_FLOAT32 p_Angle, const Vector3 &p_Axis )
 		{
 			ZED_FLOAT32 Cos = 0.0f, Sin = 0.0f;
-
 			SinCos( p_Angle, Sin, Cos );
+
+			ZED_FLOAT32 Tan = 1.0f - Cos;
+
+			Vector3 nAxis = p_Axis;
+
+			// Intermediate values
+			ZED_FLOAT32 TanX = Tan*nAxis[ 0 ];
+			ZED_FLOAT32 TanY = Tan*nAxis[ 1 ];
+			ZED_FLOAT32 TanZ = Tan*nAxis[ 2 ];
+
+			ZED_FLOAT32 SinX = Sin*nAxis[ 0 ];
+			ZED_FLOAT32 SinY = Sin*nAxis[ 1 ];
+			ZED_FLOAT32 SinZ = Sin*nAxis[ 2 ];
 		}
 	}
 }
--- Main/Source/XNA/Global.cs
+++ Main/Source/XNA/Global.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
-namespace ZED
+public class Global
 {
-    public class Global
-    {
-        public const uint ZED_OK     = 0x00000001;
-    }
+    public static uint ZED_OK     = 0x00000001;
+
+    public static Color Goom = new Color( 0.2f, 0.0f, 0.2f );
 }
