943c2f32ad82a3696d0fa5e5284267790164d968
teh_programerer <devnull@localhost>
2010-07-26 02:35:30 +0100

    Added XNA project files not included in last commit.  Removed ZED.csproj, as ZED_{W|X} exist.

---
+++ Main/Source/XNA/Global.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+// Enclose in a ZED namespace?
+public class Global
+{
+    public static uint ZED_OK     = 0x00000001;
+
+    public static Color Goom = new Color( 0.2f, 0.0f, 0.2f );
+}
