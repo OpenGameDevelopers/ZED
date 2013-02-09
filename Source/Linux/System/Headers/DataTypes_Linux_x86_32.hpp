383afd5181edfc9e32b9bff79d1177818554ca73
Rico <teh_programerer@interactivelaboratories.com>
2013-02-09 14:50:59 +0000

    |SOURCE| [Linux::DataTypes] * The 32-bit architecture is including cstddef for size_t and all signed   integers are prefixed with 'S' * The 64-bit architecture is including cstddef for size_t

--- Source/Linux/System/Headers/DataTypes_Linux_x86_32.hpp
+++ Source/Linux/System/Headers/DataTypes_Linux_x86_32.hpp
@@ -5,21 +5,25 @@
 #include <stdint.h>
 // size_t
 #include <linux/types.h>
+#include <cstddef>
 
 typedef signed char			ZED_CHAR8;
 typedef unsigned char		ZED_UCHAR8;
-typedef signed char			ZED_INT8;
+typedef signed char			ZED_SINT8;
 typedef unsigned char		ZED_UINT8;
-typedef signed short		ZED_INT16;
+typedef signed short		ZED_SINT16;
 typedef unsigned short		ZED_UINT16;
-typedef signed int			ZED_INT32;
+typedef signed int			ZED_SINT32;
 typedef unsigned int		ZED_UINT32;
-typedef int64_t				ZED_INT64;
+typedef int64_t				ZED_SINT64;
 typedef uint64_t			ZED_UINT64;
 
 // Define the platform as x86 Endian (Little)
 #define ZED_BIG_ENDIAN		0
 #define ZED_LITTLE_ENDIAN	1
+
+#define ZED_64BIT	0
+#define ZED_32BIT	1
 
 #endif
 
