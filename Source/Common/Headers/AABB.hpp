0fca51ac2fa78a0091f9b54f9d4ad3ba0e000edd
Rico <teh_programerer@interactivelaboratories.com>
2013-04-29 01:36:37 +0000

    |SOURCE| [Linux::Arithmeitc::AABB] * Implemented Intersect

--- Source/Common/Headers/AABB.hpp
+++ Source/Common/Headers/AABB.hpp
@@ -11,6 +11,10 @@ namespace ZED
 		{
 		public:
 			ZED_BOOL Intersects( const AABB &p_Other ) const;
+			ZED_INLINE void Min( const Vector3 &p_Min )
+				{ m_Min = p_Min; }
+			ZED_INLINE void Max( const Vector3 &p_Max )
+				{ m_Max = p_Max; }
 
 		private:
 			Vector3 m_Min;
