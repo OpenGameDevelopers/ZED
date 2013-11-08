aad82f6cdb4346beb0ec3fd4f6cf9cbcb2d86678
Rico <teh_programerer@interactivelaboratories.com>
2013-11-08 16:35:14 +0000

    |SOURCE| [Common::Arithmetic::Quaternion] * Component constructor and Set member function expect w, x, y, z, instead of:   x, y, z, w * Default constructor sets the W member variable to one, creating a quaternion   with no rotation * Added multiply and assign-multiply operators * Normalise added, will modify the existing quaternion * Magnitude and MagnitudeSq added, will not modify the existing quaternion * Conjugate will return the quaternion's conjugate, while not modifying the   current quaternion
    [Linux::Arithmetic::Quaternion]
    * Magnitude returns the length of the quaternion
    * MagnitudeSq returns the squared length of the quaternion
    * Normalise modifys the quaternion, turning it into a normalised version of
      itself
    * AsVector is stubbed out
    * IsUnit is stubbed out
    * Assignemnt operator is stubbed out
    * Copy constructor is stubbed out
    * Multiply will return the product of the current quaternion mulitplied by
      another quaternion without modifying the current one
    * Assign-multiply will return the product of the current quaternion multiplied
      by another quaternion, storing the product in the current one

--- Source/Common/Headers/Arithmetic/Quaternion.hpp
+++ Source/Common/Headers/Arithmetic/Quaternion.hpp
@@ -12,7 +12,7 @@ namespace ZED
 		{
 		public:
 			ZED_INLINE Quaternion( ) : m_X( 0.0f ),
-				m_Y( 0.0f ), m_Z( 0.0f ), m_W( 0.0f ){ }
+				m_Y( 0.0f ), m_Z( 0.0f ), m_W( 1.0f ){ }
 
 			Quaternion( const Quaternion &p_Other );
 			Quaternion &operator=( const Quaternion &p_Other );
@@ -20,8 +20,8 @@ namespace ZED
 			ZED_INLINE ~Quaternion( ){ }
 
 			// Component constructor
-			ZED_INLINE Quaternion( ZED_FLOAT32 p_X, ZED_FLOAT32 p_Y,
-				ZED_FLOAT32 p_Z, ZED_FLOAT32 p_W ) : m_X( p_X ), m_Y( p_Y ),
+			ZED_INLINE Quaternion( ZED_FLOAT32 p_W, ZED_FLOAT32 p_X,
+				ZED_FLOAT32 p_Y, ZED_FLOAT32 p_Z ) : m_X( p_X ), m_Y( p_Y ),
 				m_Z( p_Z ), m_W( p_W ){ }
 			// Compound constructor
 			ZED_INLINE Quaternion( ZED_FLOAT32 p_W, const Vector3 &p_Vec ) :
@@ -29,15 +29,28 @@ namespace ZED
 				m_Z( p_Vec.Z( ) ){ }
 
 			// Set the quaternion component-wise
-			ZED_INLINE void Set( const ZED_FLOAT32 p_X, const ZED_FLOAT32 p_Y,
-				const ZED_FLOAT32 p_Z, const ZED_FLOAT32 p_W )
+			ZED_INLINE void Set( const ZED_FLOAT32 p_W, const ZED_FLOAT32 p_X,
+				const ZED_FLOAT32 p_Y, const ZED_FLOAT32 p_Z )
 				{ m_X = p_X; m_Y = p_Y; m_Z = p_Z; m_W = p_W; }
 
 			// Returns the Quaternion as a Vector3
 			ZED::Arithmetic::Vector3 AsVector( );
 
+			void Normalise( );
+
+			ZED_FLOAT32 Magnitude( ) const;
+			ZED_FLOAT32 MagnitudeSq( ) const;
+
+			Quaternion Conjugate( ) const;
+
 			// Returns true if the quaternion is of unit length
 			ZED_BOOL IsUnit( ) const;
+
+			// Non-destructive operators
+			Quaternion operator*( const Quaternion &p_Other ) const;
+			
+			// Destructive operators
+			Quaternion &operator*=( const Quaternion &p_Other );
 
 			// Easy access to the elements of the Quaternion
 			ZED_INLINE ZED_FLOAT32 &operator[ ]( const ZED_UINT32 p_Index )
