26c094f136eb643296335ae742433571b1358b55
Rico <teh_programerer@interactivelaboratories.com>
2013-11-25 20:44:30 +0000

    |SOURCE| [Common::System::File] * Added File class which will handle opening, reading, writing, and navigating   files [Common::System::NativeFile] * Added NativeFile which will access a file from storage like any other file,   without any meta data [Linux::System::NativeFile] * Open   * Uses the flags for the Access type to determine how to construct the mode     parameter for fopen   * Returns ZED_OK if the file was successfully opened or ZED_FAIL if it could     not be opened   * More specific information about any possible failures is printed to the     terminal * Close   * Checks if the file is open, if it is then it is closed * ReadByte   * Reads in the specified amount of bytes to the data pointer passed in to the     member function   * Returns ZED_FAIL if the length read does not match what was requested

--- Source/Common/Headers/System/File.hpp
+++ Source/Common/Headers/System/File.hpp
@@ -7,9 +7,36 @@ namespace ZED
 {
 	namespace System
 	{
+		const ZED_UINT32 FILE_ACCESS_READ	= 0x00000001;
+		const ZED_UINT32 FILE_ACCESS_WRITE	= 0x00000002;
+		const ZED_UINT32 FILE_ACCESS_APPEND	= 0x00000004;
+		const ZED_UINT32 FILE_ACCESS_UPDATE	= 0x00000008;
+		const ZED_UINT32 FILE_ACCESS_BINARY	= 0x00000010;
+		const ZED_UINT32 FILE_ACCESS_TEXT	= 0x00000020;
+
 		// The first parameter contains the absolute path to the executable
 		// if this function succeeds
 		ZED_UINT32 GetExecutablePath( char **p_ppBuffer, ZED_MEMSIZE p_Size );
+
+		class File
+		{
+		public:
+			virtual ~File( ){ }
+
+			virtual ZED_UINT32 Open( const ZED_CHAR8 *p_pFileName,
+				const ZED_UINT32 p_Access ) = 0;
+			virtual ZED_UINT32 Close( ) = 0;
+
+			virtual ZED_UINT32 Seek( const ZED_MEMSIZE p_Offset,
+				const ZED_UINT32 p_Origin ) = 0;
+			virtual ZED_UINT32 Rewind( );
+
+			virtual ZED_UINT32 WriteByte( const ZED_BYTE *p_pData,
+				const ZED_MEMSIZE p_Length, ZED_MEMSIZE *p_pWritten ) = 0;
+
+			virtual ZED_UINT32 ReadByte( ZED_BYTE *p_pData,
+				const ZED_MEMSIZE p_Length, ZED_MEMSIZE *p_pRead ) = 0;
+		};
 	}
 }
 
