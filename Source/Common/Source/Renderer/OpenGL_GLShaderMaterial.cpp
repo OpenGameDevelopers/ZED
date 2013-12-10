619a2dd6bf83c16267c27d0dff9be82a425da372
Rico <teh_programerer@interactivelaboratories.com>
2013-12-10 17:48:13 +0000

    |SOURCE| [Common::Renderer::GLShaderMaterial] * Declared the basic interface for GLShaderMaterial

---
+++ Source/Common/Source/Renderer/OpenGL_GLShaderMaterial.cpp
@@ -0,0 +1,13 @@
+#if defined ZED_PLATFORM_SUPPORTS_OPENGL
+
+#include <Renderer/OGL/GLShaderMaterial.hpp>
+
+namespace ZED
+{
+	namespace Renderer
+	{
+	}
+}
+
+#endif // ZED_PLATFORM_SUPPORTS_OPENGL
+
