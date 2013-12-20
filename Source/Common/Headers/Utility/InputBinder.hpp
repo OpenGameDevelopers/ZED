f595bcdf73a6d6c0f984a74e4b5ea028ecf7fed1
Rico <teh_programerer@interactivelaboratories.com>
2013-12-20 13:32:16 +0000

    |SOURCE| [Common::System::InputTypes] * ZED_MOUSE_COORDINATE_AXIS re-named to ZED_MOUSE_AXIS as it's a better   representation of what it implies [Common::Utility::InputBinder] * BindKey accepts a ZED_KEY and a ZED_UINT32, adding them to the m_KeyToAction   map, ZED_FAIL is returned if the key and action pair could not be inserted   into the map * GetActionFromKey accepts a ZED_KEY which will be used to find the action for   the key in the m_KeyToAction map.  Zero is returned on failure to find an   action for the key * The map types have been typedef'd which will lessen any typing errors and   reduce compile-time errors

--- Source/Common/Headers/Utility/InputBinder.hpp
+++ Source/Common/Headers/Utility/InputBinder.hpp
@@ -14,15 +14,29 @@ namespace ZED
 			InputBinder( );
 			~InputBinder( );
 
+			ZED_UINT32 BindKey( const ZED_KEY p_Key,
+				const ZED_UINT32 p_Action );
+
+			ZED_UINT32 GetActionFromKey( const ZED_KEY p_Key ) const;
+
 		private:
+			// Keyboard
+			typedef std::map< ZED_KEY, ZED_UINT32 > KeyMap;
+			typedef std::pair< ZED_KEY, ZED_UINT32 > KeyMapEntry;
+			typedef std::pair< KeyMap::iterator, bool > KeyMapInsertResult;
+			// Mouse
+			typedef std::map< ZED_MOUSE_BUTTON, ZED_UINT32 > MouseButtonMap;
+			typedef std::map< ZED_MOUSE_AXIS, ZED_UINT32 > MouseAxisMap;
+			// Gamepad
+			typedef std::map< ZED_GAMEPAD, ZED_UINT32 > GamepadMap;
+
 			InputBinder( const InputBinder & );
 			InputBinder &operator=( const InputBinder & );
 
-			std::map< ZED_KEY, ZED_UINT32 > m_KeyToAction;
-			std::map< ZED_MOUSE_BUTTON, ZED_UINT32 > m_MouseButtonToAction;
-			std::map< ZED_MOUSE_COORDINATE_AXIS, ZED_UINT32 >
-				m_MouseCoordinateAxisToAction;
-			std::map< ZED_GAMEPAD, ZED_UINT32 > m_GamepadToAction;
+			KeyMap			m_KeyToAction;
+			MouseButtonMap	m_MouseButtonToAction;
+			MouseAxisMap	m_MouseCoordinateAxisToAction;
+			GamepadMap		m_GamepadToAction;
 		};
 	}
 }
