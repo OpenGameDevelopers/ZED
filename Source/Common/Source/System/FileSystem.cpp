9d132cd755eb5a06fbe0a012332237eca3d508e8
RedRingRico <teh_programerer@interactivelaboratories.com>
2014-12-15 14:42:20 +0000

    Started work on a file system
    |SOURCE|
    [Common/System/FileSystem]
    * The file system conceptualises all files as a single directory full of files,
      capable of holding files in archives and single files on mass storage
    * File containers will handle extracting the information out of different
      archive file formats
    * OpenFile has some commented-out pseudocode to illustrate the concept

---
+++ Source/Common/Source/System/FileSystem.cpp
@@ -0,0 +1,66 @@
+#include <System/FileSystem.hpp>
+#include <System/File.hpp>
+#include <string>
+
+namespace ZED
+{
+	namespace System
+	{
+		FileSystem::FileSystem( )
+		{
+		}
+
+		FileSystem::~FileSystem( )
+		{
+		}
+
+		File *FileSystem::OpenFile( const ZED_CHAR8 *p_pFileName )
+		{
+			if( p_pFileName == ZED_NULL )
+			{
+				return ZED_NULL;
+			}
+
+			std::string FileName = p_pFileName;
+
+			// Extract the URI (if there is one)
+			ZED_MEMSIZE ColonLocation = FileName.find_first_of( ':' );
+
+			if( ColonLocation == std::string::npos )
+			{
+				// Try and load one of the files in the file system
+			}
+
+			/*
+			std::vector< FileContainer * >::iterator Container 
+				= m_FileContainerArray.begin( );
+
+			std::string URI = FileName.substr( 0, ColonLocation );
+
+			while( Container != m_FileContainerArray.end( ) )
+			{
+				if( URI.compare( ( *Container )->GetName( ) ) == 0 )
+				{
+					break;
+				}
+				++Container;
+			}
+
+			if( Container == m_FileContainerArray.end( ) )
+			{
+				return ZED_NULL;
+			}
+
+			std::vector< File > FileArray;
+
+			( *Container )->ExtractFiles( FileArray );
+
+			for each file in FileArray
+				compare file names and their checksums
+					if the checksums match, continue
+					else return null, as duplicate file names with different data are not permitted
+			*/
+		}
+	}
+}
+
